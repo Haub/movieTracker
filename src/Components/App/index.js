987f8110d8b5ff1aa15b137abc76a814a810558a
Graham <g@masterofnone.com>
2018-10-22 15:51:59 -0600

    Setup API to fetch, clean and return data to APP

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { getMovies } from '../Utils/API'
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      login: false,
+      data: []
+    }
+  }
+
+  async componentDidMount() {
+    const data = await getMovies();
+    this.setState( { data })
+  }
+
+
   render() {
     return (
       <div className="App">
-        
+        HEY
       </div>
     );
   }
