7c3a01287acd68eebb0bd9dcba846119caf9472b
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-24 15:18:46 -0600

    Add reducers folder with moviesReducer

---
+++ src/reducers/moviesReducer.js
@@ -0,0 +1,10 @@
+import { addMovies } from '../actions/addMovies';
+
+export const moviesReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_MOVIES':
+      return action.movies
+    default: 
+      return state
+  }
+}
