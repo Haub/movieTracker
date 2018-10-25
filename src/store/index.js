e33aee1d094fc0863a73cf6067733d0a81203767
Graham <g@masterofnone.com>
2018-10-25 15:36:47 -0600

    Refactor redux store for middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import { createStore,  applyMiddleware, compose } from 'redux';
-import { rootReducer } from '../reducers';
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { rootReducer } from '../reducers'
+import thunk from "redux-thunk";
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ / window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 export const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
     devTools
   )
-  );
+)
