1115f84fb831e342214a89ff92600e95c79602c4
Graham <g@masterofnone.com>
2018-10-25 15:35:28 -0600

    Add actions to handle async fetch in redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,47 @@
-export const FetchMovieUser = ()
+import { getMovies, getUser, addUser } from "../utils";
 
+export const fetchMovies = () => {
+  return async dispatch => {
+    dispatch(contentStatus('loading'))
+    try {
+      const response = await getMovies()
+      dispatch(addMovies(response))
+      dispatch(contentStatus('resolved'))
+    } catch (error) {
+      dispatch(contentStatus('error'))
+    }
+  }
+}
 
+export const fetchUser = (name, email, password, avatar) => {
+  return async dispatch => {
+    dispatch(contentStatus('loading'))
+    try {
+      let response;
+      if (!name) {
+        response = await getUser(email, password)
+      } else {
+        response = await addUser(name, email, password, avatar)
+      }
+      dispatch(loginUser(response))
+      dispatch(contentStatus('resolved'))
+    } catch (error) {
+      dispatch(contentStatus('error'))
+    }
+  }
+}
 
+const contentStatus = (string) => ({
+  type: 'CONTENT_STATUS',
+  status: string
+})
 
-export const addMovies = (movies) => ({
+const addMovies = (movies) => ({
   type: "ADD_MOVIES",
   movies
 });
 
-export const loginUser = (user) => ({
+const loginUser = (user) => ({
   type: 'LOGIN_USER',
   user
 })
