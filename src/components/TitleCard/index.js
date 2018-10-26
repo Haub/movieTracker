adcc57d8f0f393b940c6a917fc3c0793a09553ca
Graham <g@masterofnone.com>
2018-10-25 22:08:22 -0600

    Setup video for title card

--- src/components/TitleCard/index.js
+++ src/components/TitleCard/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './TitleCard.css';
-// import VideoPlayer from '../VideoPlayer';
+import VideoPlayer from '../VideoPlayer';
 
 export const TitleCard = (props) => {
- 
   return(
     <div className='title-card' >
       <div className='foreground'>
       </div>
-      {/* <VideoPlayer image={props.background}   /> */}
+      <VideoPlayer image={props.image}   url={props.video[0].key} />
     </div>
 
   )
