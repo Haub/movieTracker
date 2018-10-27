b76a6b0aba8adf88952708de91e87ec7df7da125
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-27 17:56:49 -0600

    Write control favorites function

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -24,18 +24,35 @@ export const postData = async (url, name, email, password) => {
   }
 }
 
-// export const postFavorites = async (id) => {
-//   try {
-//     const response = await fetch(url, {
-//       method: 'POST',
-//       body: JSON.stringify({name: name, email: email, password: password }),
-//       headers: {
-//         "Content-Type": "application/json"
-//       }
-//     })
-//     const user = await response.json();
-//     return user
-//   } catch(error) {
-//     throw new Error(error.message)
-//   }
-// }
+export const addFavorite = async (movie, url) => {
+  console.log(movie)
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const favoriteId = await response.json();
+    return favoriteId
+  } catch(error) {
+    throw new Error(error.message)
+  }
+}
+
+export const removeFavorite = async (movie, url) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({ user_id: movie.userId, movie_id: movie.id }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    const favoriteId = await response.json();
+    return favoriteId
+  } catch(error) {
+    throw new Error(error.message)
+  }
+}
