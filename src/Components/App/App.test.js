1b9c642c37eafd25e2de710a10a20998d4cfd068
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-27 15:31:34 -0600

    Add tests for App, mapStateToProps, mapDispatchToProps

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './index.js';
+import App, { mapStateToProps, mapDispatchToProps } from './index.js';
+import { shallow, enzyme } from 'enzyme';
+import { fetchMovies } from '../../actions';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+
+describe('APP', () => {
+  it('should call fetchMovies on componentDidMount', () => {
+    let mockFetch = jest.fn().mockImplementation(() => (movies));
+    wrapper = shallow(
+      <App
+        fetchMovies = {mockFetch}
+      />
+    )
+    let movies = [];
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        json: () => Promise.resolve(movies)
+      });
+    });
+
+    })
 });
+
+describe('mapStateToProps', () => {
+  it('should have access to movies and current user', () => {
+    const mockStore = {
+      movies: [],
+      user: {}
+    }
+    const expected = {...mockStore};
+    const result = mapStateToProps(mockStore);
+    expect(result).toEqual(expected);
+  });
+})
+
+describe('mapDispatchToProps', () => {
+  it('should display the movies when fetchMovies is invoked', () => {
+    const mockDispatch = jest.fn();
+    const movies = [];
+    const action = fetchMovies(movies);
+    const props = mapDispatchToProps(mockDispatch);
+    props.fetchMovies([]);
+    expect(mockDispatch).toHaveBeenCalledWith(action);
+  });
+})
