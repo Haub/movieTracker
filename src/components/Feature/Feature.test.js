d54912e7967515a99c706f3f9fb942d4bb7a751a
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-30 10:44:28 -0600

    Pass tests for feature

--- src/components/Feature/Feature.test.js
+++ src/components/Feature/Feature.test.js
@@ -1,3 +1,26 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import { Feature} from './index.js';
 
-describe('Feature ')
+describe('FEATURE', () => {
+  it('should match the snapshot', () => {
+    const mockMovies = [];
+    const wrapper = shallow(<Feature movies={mockMovies} />)
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should have initial state of play set to false', () => {
+    const mockMovies = [];    
+    const wrapper = shallow(<Feature movies={mockMovies} />) 
+    expect(wrapper.state().play).toEqual(false);
+  })
+   
+})
+
+describe('mapStateToProps', () => {
+
+})
+
+describe('mapDispatchToProps', () => {
+
+})
