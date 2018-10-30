7f5bc7ddeb610c0a4c8a13cdc28ff1abcefc6d2c
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-10-30 10:52:27 -0600

    Add mapStateToProps and mapDispatchToProps tests for feature

--- src/components/Feature/Feature.test.js
+++ src/components/Feature/Feature.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Feature} from './index.js';
+import { mapStateToProps, mapDispatchToProps } from './index.js';
+import * as Actions from '../../actions';
+
 
 describe('FEATURE', () => {
   it('should match the snapshot', () => {
@@ -18,9 +21,25 @@ describe('FEATURE', () => {
 })
 
 describe('mapStateToProps', () => {
-
+  it('should have access to movies and current user', () => {
+    const mockStore = {
+      movies: [],
+      user: {}
+    }
+    const expected = {...mockStore};
+    const result = mapStateToProps(mockStore);
+    expect(result).toEqual(expected);
+  });
 })
 
 describe('mapDispatchToProps', () => {
-
+  it('should call dispatch when fetchMovies is invoked', () => {
+    const mockDispatch = jest.fn();
+    const mockFetchMovies = jest.fn();
+    const movie = [{title: 'Venom'}];
+    const action = Actions.controlFavorites(movie);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.controlFavorites({title: 'Venom'});
+    expect(mockDispatch).toHaveBeenCalled();
+  });
 })
