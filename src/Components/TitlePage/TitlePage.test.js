36d5e34276c1b21e5b770bb3eca7a2df3509e1c4
bhayek12@gmail.com <bhayek12@gmail.com>
2018-10-29 21:02:15 -0600

    Add snapshot for TitlePage

---
+++ src/Components/TitlePage/TitlePage.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TitlePage } from './index';
+import { shallow } from 'enzyme';
+
+describe('TitlePage', () => {
+let wrapper;
+let movie;
+  
+  beforeEach(() => {
+    wrapper = shallow(<TitlePage movie={{movie}} />);
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+})
