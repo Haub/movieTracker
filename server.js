62d67f4294d5370d1b10a45c83b521cd5924d713
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-15 14:16:53 -0700

    Fix all endpoints

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
-const db = require('knex')(config)
+const database = require('knex')(config)
 const app = express()
 const cors = require('cors');
 
@@ -12,89 +12,100 @@ app.use(express.static('public'));
 
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'movieTracker';
+app.locals.title = 'users';
+
+  app.get('/api/users', (request, response) => {
+    database('users').select()
+        .then(user => {
+            response.status(200).json(user)
+        })
+        .catch(error => {
+            response.status(500).json({ error: error.message });
+        })
+  })
+
+  app.post('/api/users/new', (request, response) => {
+    const user = request.body
+  
+    for(let requiredParam of ['name', 'email', 'password']) {
+        if(!user[requiredParam]) {
+            response.status(422).json({error: error.message})
+        }
+    }
+  
+    database('users').insert(user, 'id')
+        .then(userIds => {
+            response.status(201).json({id: userIds[0]})
+        })
+        .catch(error => {
+            response.status(500).json({error: error.message})
+        })
+  })
+
+  app.post('/api/users', (request, response) => {
+    const user = request.body
+  
+    for(let requiredParam of ['email', 'password']) {
+        if(!user[requiredParam]) {
+            response.status(422).json({error: error.message})
+        }
+    }
+  
+    database('users').where('email', user.email)
+        .select()
+        .then(userIds => {
+            response.status(201).json(userIds[0])
+        })
+        .catch(error => {
+            response.status(500).json({error: error.message})
+        })
+  })
+
+  app.post('/api/users/favorites/new', (request, response) => {
+    const favorite = request.body
+  
+    for(let requiredParam of ['movie_id', 'user_id', 'title', 'poster_path', 'release_date', 'vote_average', 'overview']) {
+        if(!favorite[requiredParam]) {
+            response.status(422).json({error: error.message})
+        }
+    }
+  
+    database('favorites').insert(favorite, 'id')
+        .then(favorites => {
+            response.status(201).json(favorites)
+        })
+        .catch(error => {
+            response.status(500).json({error: error.message})
+        })
+  })
+
+  app.get('/api/users/:id/favorites', (request, response) => {
+    const { id } = request.params
+    
+    database('favorites').where('user_id', id).select()
+        .then(favorite => response.status(200).json(favorite))
+        .catch(error => response.status(500).json(`Error fetching favorite: ${error.message}`))
+  })
+  
+  app.delete('/api/users/:id/favorites/:movie_id', (request, response) => {
+    const { id, movie_id } = request.params
+    
+    database('favorites').where('id', movie_id).del()
+    .then(favorite => {
+      response.status(201).json(id)
+    })
+    .catch(error => {
+      response.status(500).json({error: error.message})
+    })
+  })
 
 
+    app.use(function (request, response, next) {
+        response.status(404).sendFile(__dirname + '/public/404.html')
+      })
+      
+    app.listen(app.get('port'), () => {
+        console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+    });
 
-function getAllUsers(req, res, next) {
-    db.any('select * from users')
-      .then(function(data) {
-        res.status(200).json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved All Users'
-        });
-      }).catch(function(err) {
-        return next(err)
-    });
-  }
-  
-  function signIn(req, res, next) {
-    db.one('select * from users where email=${email} and password=${password}', req.body)
-    .then(function (data) {
-    res.status(200)
-      .json({
-        status: 'success',
-        data: data,
-        message: 'Retrieved ONE User'
-      });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-  }
-  
-  function createUser(req, res, next) {
-    req.body.email = req.body.email.toLowerCase();
-    db.one('insert into users(name, password, email)' + 'values(${name}, ${password}, ${email}) returning id', req.body).then(function(data) {
-      res.status(200).json({ status: 'success', message: "New user created", id: data.id});
-    }).catch(function(err) {
-      res.status(500).json({error: err.detail});
-    })
-  }
-  
-  function addFavorite(req, res, next) {
-    db.one('insert into favorites(movie_id, user_id, title, poster_path, release_date, vote_average, overview)' +
-    'values(${movie_id}, ${user_id}, ${title}, ${poster_path}, ${release_date}, ${vote_average}, ${overview}) returning id', req.body)
-    .then(function(data) {
-      res.status(200).json({ status: 'success', message: "Movie was added to favorites", id: data.id});
-    }).catch(function(err) {
-      next(err);
-    })
-  }
-  
-  function getAllFavorites(req, res, next) {
-    var user_id = parseInt(req.params.id);
-    db.any('select * from favorites where user_id=$1', user_id)
-    .then(function(data) {
-      res.status(200).json({
-        status: 'success',
-        data: data,
-        message: 'Retrieved All favorites'
-      });
-    })
-    .catch(function(err) {
-      return next(err);
-    });
-  }
-  
-  function deleteFavorite(req, res, next) {
-    var movie_id = parseInt(req.params.movie_id);
-    var user_id = parseInt(req.params.id);
-    db.result('delete from favorites where user_id = $1 and movie_id = $2', [user_id, movie_id]).then(function(result) {
-      res.status(200)
-      .json({status: 'success', message: `${result.rowCount} row was deleted.`})
-    })
-    .catch(function(err) {
-      return next(err);
-    })
-  }
-  
-  module.exports = {
-    getAllUsers: getAllUsers,
-    signIn: signIn,
-    createUser: createUser,
-    getAllFavorites: getAllFavorites,
-    addFavorite: addFavorite,
-    deleteFavorite: deleteFavorite
-  };
+    module.exports = app
